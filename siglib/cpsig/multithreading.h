ad816fe8fcbf2ae908b8b79e6496fc42327b61d2
daniil-shmelev <164039932+daniil-shmelev@users.noreply.github.com>
2026-04-03 21:11:38 +0100

    Optimised sig kernel on CPU

--- siglib/cpsig/multithreading.h
+++ siglib/cpsig/multithreading.h
@@ -22,6 +22,8 @@ inline unsigned int get_max_threads() {
 	return max_threads;
 }
 
+// Contiguous chunk assignment: each thread processes a contiguous block
+// of batch items for better cache locality.
 template<typename T, typename U, typename FN>
 void multi_threaded_batch(FN& thread_func, T* path, U* out, uint64_t batch_size, uint64_t flat_path_length, uint64_t result_length, int n_jobs) {
 	if (n_jobs == 0)
@@ -29,30 +31,25 @@ void multi_threaded_batch(FN& thread_func, T* path, U* out, uint64_t batch_size,
 	const int max_threads = n_jobs > 0 ? n_jobs : get_max_threads() + 1 + n_jobs;
 	if (max_threads < 1)
 		throw std::invalid_argument("received negative n_jobs which is less than max_threads + 1; n_jobs too low");
-	const uint64_t thread_path_step = flat_path_length * max_threads;
-	const uint64_t thread_result_step = result_length * max_threads;
-	T* const data_end = path + flat_path_length * batch_size;
 
+	const uint64_t num_threads = std::min(static_cast<uint64_t>(max_threads), batch_size);
 	std::vector<std::thread> workers;
 
-	auto batch_thread_func = [&](T* path_ptr, U* out_ptr) {
-		U* out_ptr_ = out_ptr;
-		for (T* path_ptr_ = path_ptr;
-			path_ptr_ < data_end;
-			path_ptr_ += thread_path_step, out_ptr_ += thread_result_step) {
-
-			thread_func(path_ptr_, out_ptr_);
+	auto batch_thread_func = [&](uint64_t start, uint64_t end) {
+		T* path_ptr = path + start * flat_path_length;
+		U* out_ptr = out + start * result_length;
+		for (uint64_t i = start; i < end; ++i, path_ptr += flat_path_length, out_ptr += result_length) {
+			thread_func(path_ptr, out_ptr);
 		}
-		};
+	};
 
-	int num_threads = 0;
-	U* out_ptr = out;
-	for (T* path_ptr = path;
-		(num_threads < max_threads) && (path_ptr < data_end);
-		path_ptr += flat_path_length, out_ptr += result_length) {
-
-		workers.emplace_back(batch_thread_func, path_ptr, out_ptr);
-		++num_threads;
+	const uint64_t chunk = batch_size / num_threads;
+	const uint64_t remainder = batch_size % num_threads;
+	uint64_t start = 0;
+	for (uint64_t t = 0; t < num_threads; ++t) {
+		uint64_t end = start + chunk + (t < remainder ? 1 : 0);
+		workers.emplace_back(batch_thread_func, start, end);
+		start = end;
 	}
 
 	for (auto& w : workers)
@@ -67,35 +64,25 @@ void multi_threaded_batch_2(FN& thread_func, S* path1, T* path2, U* out, uint64_
 	const int max_threads = n_jobs > 0 ? n_jobs : get_max_threads() + 1 + n_jobs;
 	if (max_threads < 1)
 		throw std::invalid_argument("received negative n_jobs which is less than max_threads + 1; n_jobs too low");
-	const uint64_t thread_path_step_1 = flat_path_length_1 * max_threads;
-	const uint64_t thread_path_step_2 = flat_path_length_2 * max_threads;
-	const uint64_t thread_result_step = result_length * max_threads;
-	S* const data_end_1 = path1 + flat_path_length_1 * batch_size;
 
+	const uint64_t num_threads = std::min(static_cast<uint64_t>(max_threads), batch_size);
 	std::vector<std::thread> workers;
 
-	auto batch_thread_func = [&](S* path_ptr_1, T* path_ptr_2, U* out_ptr) {
-		U* out_ptr_ = out_ptr;
-		S* path1_ptr_ = path_ptr_1;
-		T* path2_ptr_ = path_ptr_2;
-		for (;
-			path1_ptr_ < data_end_1;
-			path1_ptr_ += thread_path_step_1, path2_ptr_ += thread_path_step_2, out_ptr_ += thread_result_step) {
+	auto batch_thread_func = [&](uint64_t start, uint64_t end) {
+		S* p1 = path1 + start * flat_path_length_1;
+		T* p2 = path2 + start * flat_path_length_2;
+		U* o = out + start * result_length;
+		for (uint64_t i = start; i < end; ++i, p1 += flat_path_length_1, p2 += flat_path_length_2, o += result_length)
+			thread_func(p1, p2, o);
+	};
 
-			thread_func(path1_ptr_, path2_ptr_, out_ptr_);
-		}
-		};
-
-	int num_threads = 0;
-	U* out_ptr = out;
-	S* path1_ptr = path1;
-	T* path2_ptr = path2;
-	for (;
-		(num_threads < max_threads) && (path1_ptr < data_end_1);
-		path1_ptr += flat_path_length_1, path2_ptr += flat_path_length_2, out_ptr += result_length) {
-
-		workers.emplace_back(batch_thread_func, path1_ptr, path2_ptr, out_ptr);
-		++num_threads;
+	const uint64_t chunk = batch_size / num_threads;
+	const uint64_t remainder = batch_size % num_threads;
+	uint64_t start = 0;
+	for (uint64_t t = 0; t < num_threads; ++t) {
+		uint64_t end = start + chunk + (t < remainder ? 1 : 0);
+		workers.emplace_back(batch_thread_func, start, end);
+		start = end;
 	}
 
 	for (auto& w : workers)
@@ -109,38 +96,26 @@ void multi_threaded_batch_3(FN& thread_func, R* path1, S* path2, T* path3, U* ou
 	const int max_threads = n_jobs > 0 ? n_jobs : get_max_threads() + 1 + n_jobs;
 	if (max_threads < 1)
 		throw std::invalid_argument("received negative n_jobs which is less than max_threads + 1; n_jobs too low");
-	const uint64_t thread_path_step_1 = flat_path_length_1 * max_threads;
-	const uint64_t thread_path_step_2 = flat_path_length_2 * max_threads;
-	const uint64_t thread_path_step_3 = flat_path_length_3 * max_threads;
-	const uint64_t thread_result_step = result_length * max_threads;
-	R* const data_end_1 = path1 + flat_path_length_1 * batch_size;
 
+	const uint64_t num_threads = std::min(static_cast<uint64_t>(max_threads), batch_size);
 	std::vector<std::thread> workers;
 
-	auto batch_thread_func = [&](R* path_ptr_1, S* path_ptr_2, T* path_ptr_3, U* out_ptr) {
-		U* out_ptr_ = out_ptr;
-		R* path1_ptr_ = path_ptr_1;
-		S* path2_ptr_ = path_ptr_2;
-		T* path3_ptr_ = path_ptr_3;
-		for (;
-			path1_ptr_ < data_end_1;
-			path1_ptr_ += thread_path_step_1, path2_ptr_ += thread_path_step_2, path3_ptr_ += thread_path_step_3, out_ptr_ += thread_result_step) {
+	auto batch_thread_func = [&](uint64_t start, uint64_t end) {
+		R* p1 = path1 + start * flat_path_length_1;
+		S* p2 = path2 + start * flat_path_length_2;
+		T* p3 = path3 + start * flat_path_length_3;
+		U* o = out + start * result_length;
+		for (uint64_t i = start; i < end; ++i, p1 += flat_path_length_1, p2 += flat_path_length_2, p3 += flat_path_length_3, o += result_length)
+			thread_func(p1, p2, p3, o);
+	};
 
-			thread_func(path1_ptr_, path2_ptr_, path3_ptr_, out_ptr_);
-		}
-		};
-
-	int num_threads = 0;
-	U* out_ptr = out;
-	R* path1_ptr = path1;
-	S* path2_ptr = path2;
-	T* path3_ptr = path3;
-	for (;
-		(num_threads < max_threads) && (path1_ptr < data_end_1);
-		path1_ptr += flat_path_length_1, path2_ptr += flat_path_length_2, path3_ptr += flat_path_length_3, out_ptr += result_length) {
-
-		workers.emplace_back(batch_thread_func, path1_ptr, path2_ptr, path3_ptr, out_ptr);
-		++num_threads;
+	const uint64_t chunk = batch_size / num_threads;
+	const uint64_t remainder = batch_size % num_threads;
+	uint64_t start = 0;
+	for (uint64_t t = 0; t < num_threads; ++t) {
+		uint64_t end = start + chunk + (t < remainder ? 1 : 0);
+		workers.emplace_back(batch_thread_func, start, end);
+		start = end;
 	}
 
 	for (auto& w : workers)
@@ -154,41 +129,27 @@ void multi_threaded_batch_4(FN& thread_func, const T* path1, T* path2, T* path3,
 	const int max_threads = n_jobs > 0 ? n_jobs : get_max_threads() + 1 + n_jobs;
 	if (max_threads < 1)
 		throw std::invalid_argument("received negative n_jobs which is less than max_threads + 1; n_jobs too low");
-	const uint64_t thread_path_step_1 = flat_path_length_1 * max_threads;
-	const uint64_t thread_path_step_2 = flat_path_length_2 * max_threads;
-	const uint64_t thread_path_step_3 = flat_path_length_3 * max_threads;
-	const uint64_t thread_path_step_4 = flat_path_length_4 * max_threads;
-	const uint64_t thread_result_step = result_length * max_threads;
-	const T* const data_end_1 = path1 + flat_path_length_1 * batch_size;
 
+	const uint64_t num_threads = std::min(static_cast<uint64_t>(max_threads), batch_size);
 	std::vector<std::thread> workers;
 
-	auto batch_thread_func = [&](const T* path_ptr_1, T* path_ptr_2, T* path_ptr_3, const T* path_ptr_4, const U* out_ptr) {
-		const U* out_ptr_ = out_ptr;
-		const T* path1_ptr_ = path_ptr_1;
-		T* path2_ptr_ = path_ptr_2;
-		T* path3_ptr_ = path_ptr_3;
-		const T* path4_ptr_ = path_ptr_4;
-		for (;
-			path1_ptr_ < data_end_1;
-			path1_ptr_ += thread_path_step_1, path2_ptr_ += thread_path_step_2, path3_ptr_ += thread_path_step_3, path4_ptr_ += thread_path_step_4, out_ptr_ += thread_result_step) {
+	auto batch_thread_func = [&](uint64_t start, uint64_t end) {
+		const T* p1 = path1 + start * flat_path_length_1;
+		T* p2 = path2 + start * flat_path_length_2;
+		T* p3 = path3 + start * flat_path_length_3;
+		const T* p4 = path4 + start * flat_path_length_4;
+		const U* o = out + start * result_length;
+		for (uint64_t i = start; i < end; ++i, p1 += flat_path_length_1, p2 += flat_path_length_2, p3 += flat_path_length_3, p4 += flat_path_length_4, o += result_length)
+			thread_func(p1, p2, p3, p4, o);
+	};
 
-			thread_func(path1_ptr_, path2_ptr_, path3_ptr_, path4_ptr_, out_ptr_);
-		}
-		};
-
-	int num_threads = 0;
-	const U* out_ptr = out;
-	const T* path1_ptr = path1;
-	T* path2_ptr = path2;
-	T* path3_ptr = path3;
-	const T* path4_ptr = path4;
-	for (;
-		(num_threads < max_threads) && (path1_ptr < data_end_1);
-		path1_ptr += flat_path_length_1, path2_ptr += flat_path_length_2, path3_ptr += flat_path_length_3, path4_ptr += flat_path_length_4, out_ptr += result_length) {
-
-		workers.emplace_back(batch_thread_func, path1_ptr, path2_ptr, path3_ptr, path4_ptr, out_ptr);
-		++num_threads;
+	const uint64_t chunk = batch_size / num_threads;
+	const uint64_t remainder = batch_size % num_threads;
+	uint64_t start = 0;
+	for (uint64_t t = 0; t < num_threads; ++t) {
+		uint64_t end = start + chunk + (t < remainder ? 1 : 0);
+		workers.emplace_back(batch_thread_func, start, end);
+		start = end;
 	}
 
 	for (auto& w : workers)
